64571b6383b7ab4774fe85023fe48b0555274191
Shashank Chavan <chavanshashank@hotmail.com>
2020-01-31 17:59:46 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 hub checkout master
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S) 
+VERSION_CODE=`date '+%Y%U%w'`
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}.${VERSION_CODE}" $(date +%Y%m%d%H%M%S) 
